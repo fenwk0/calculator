40f1dee7bcc8fa2396be51099cafcfe93512e57f
fenwk0 <karl.fenwick@gmail.com>
2018-10-02 14:32:37 +0100

    Added new Chef app

--- src/main/java/com/unit/test/cooking/Chef.java
+++ src/main/java/com/unit/test/cooking/Chef.java
@@ -2,7 +2,7 @@ package com.unit.test.cooking;
 
 public class Chef {
     public String cook(Recipe recipe) {
-        String result = "Chef.cook" + recipe.getCookingBehaviour().getCookingMethod() +" an " + recipe.getFood().getName();
+        String result = "Chef.cook : " + recipe.getCookingBehaviour().getCookingMethod() +" an " + recipe.getFood().getName();
         return result;
     }
 }
